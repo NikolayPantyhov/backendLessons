11475edb45a2dc3fe8616abc0be81765a10c3222
NikolayPantyhov <147746217+NikolayPantyhov@users.noreply.github.com>
2024-05-21 23:53:34 +0300

    homework lesson3

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ criticalLog('critical message', __filename)
 warningLog('warning message', __filename)
 debugLog('debug message', __filename)
 
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i)
+  }, i * 100)
+}
+
+for (var i = 0; i < 10; i++) {
+  ((i) => {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 100);
+  })(i);
+}
